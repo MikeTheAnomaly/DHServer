c9578bdffafc958d963a433c02810a01974a98f4
michael.hoff <michael.hoff@HU130H112>
2019-12-02 21:18:42 -0600

    yeah

--- ServerDH/src/serverdh/Server.java
+++ ServerDH/src/serverdh/Server.java
@@ -2,12 +2,17 @@
  * Server example used from "https://www.geeksforgeeks.org/socket-programming-in-java/" 
  * by Souradeep Barua
  */
+
+//https://stackoverflow.com/questions/38271609/client-server-porgram-file-encrypt-at-server-and-decrypt-at-client-java
+//https://www.geeksforgeeks.org/socket-programming-in-java/
+//https://stackoverflow.com/questions/9520911/java-sending-and-receiving-file-byte-over-sockets
+//https://stackoverflow.com/questions/309424/how-do-i-read-convert-an-inputstream-into-a-string-in-java
 package serverdh;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  *
@@ -17,54 +22,33 @@ public class Server {
 
     //initialize socket and input stream 
     private Socket socket = null;
-    private ServerSocket server = null;
-    private DataInputStream in = null;
+    private ServerSocket serverSocket = null;
+    private InputStream in = null;
+    private OutputStream out = null;
+    private BufferedReader br = null;
 
     // constructor with port 
-    public Server(int port) throws IOException{
-        // starts server and waits for a connection 
-            server = new ServerSocket(port);
-            System.out.println("Server started on port: " + port);
+    public Server(int port) throws IOException {
 
-            System.out.println("Waiting for a client ...");
-            
-            socket = server.accept();
-            System.out.println("Client accepted" + socket.getRemoteSocketAddress().toString());
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            System.out.println(socket.getInputStream().toString());
-            // takes input from the client socket 
-            in = new DataInputStream(
-                    new BufferedInputStream(socket.getInputStream()));
+        serverSocket = new ServerSocket(port);
+        socket = serverSocket.accept();
+        System.out.println("Connected to Client");
+        in = socket.getInputStream();
+        out = socket.getOutputStream();
 
-            String line = "";
+        byte[] bytes = new byte[16 * 1024];  
+        //Put in seprate socket for sending data where in is fileinputstream
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            Scanner s = new Scanner(in).useDelimiter("\\A");
+            String result = s.hasNext() ? s.next() : "";
+            System.out.println(result);
+            out.write(bytes, 0, count);
+        }
 
-            // reads message from client until "Over" is sent 
-            while (!line.equals("Over")) {
-                try {
-                    line = in.readUTF();
-                    System.out.println(line);
-
-                } catch (IOException i) {
-                    System.out.println(i);
-                }
-            }
-            System.out.println("Closing connection");
-
-            // close connection 
-            socket.close();
-            in.close();
+        out.close();
+        in.close();
+        socket.close();
+        serverSocket.close();
     }
 }
